7b266e1657bad66962e41a737db9ddf7c5590a02
Justyna Barut <justyna.barut@gmail.com>
2017-02-04 17:43:47 +0100

    add form to new comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @post = @comment.post
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @post, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { redirect_to @post }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :text, :date, :post_id)
+  end
+end
