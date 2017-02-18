f2a22b1953e2c599f0eb17e94a8ce924109763ea
Justyna Barut <justyna.barut@gmail.com>
2017-02-18 16:36:10 +0100

    add static page o mnie

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resource :about, only: :show
   resources :posts
   resources :comments, only: :create
   root 'posts#index'
+  # get ''
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
