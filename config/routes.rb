7f4d35b0d42473a2a5947807e244496813e62b02
Justyna Barut <justyna.barut@gmail.com>
2017-02-18 17:18:45 +0100

    implemented changes from pull request

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   resources :posts
   resources :comments, only: :create
   root 'posts#index'
-  # get ''
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
